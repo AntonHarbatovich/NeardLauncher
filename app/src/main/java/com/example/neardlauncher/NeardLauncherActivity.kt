92d9bd052653f0f31cf8036004a208c2a13e2959
Anton Harbatovich <a.harbatovich@gmail.com>
2022-04-18 15:30:02 +0300

    Initial commit

---
+++ app/src/main/java/com/example/neardlauncher/NeardLauncherActivity.kt
@@ -0,0 +1,11 @@
+package com.example.neardlauncher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NeardLauncherActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_neard_launcher)
+    }
+}
